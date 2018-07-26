984a73ff6deb939d5dcde090c339702e620bd72d
Adrian <adrian971029@gmail.com>
2018-07-26 17:10:24 -0300

    Commit inicial add libJavaPiadas

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,54 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.adrian_971029.libjavapiadas.Piadas;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Piadas mPiadas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPiadas = new Piadas();
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        Toast.makeText(this,mPiadas.ruimPiada1(), Toast.LENGTH_SHORT).show();
+    }
+
+
+}
---
+++ libJavaPiadas/src/main/java/com/adrian_971029/libjavapiadas/Piadas.java
@@ -0,0 +1,57 @@
+package com.adrian_971029.libjavapiadas;
+
+public class Piadas {
+
+    //Piadas boas obtidas em : https://www.piadasnet.com/piadas-curtas.htm
+    //Piadas ruims obtidas em : https://muitobacana.com/piadas-ruins-e-merdas/
+
+
+    public Piadas() {
+    }
+
+    public String boaPiada1() {
+        return "Mamae, mamae... me leva no circo? Nao, filho... Se querem te ver, que venham aqui em casa...";
+    }
+
+    public String boaPiada2() {
+        return "O garoto apanhou da vizinha, e a mãe furiosa foi tomar satisfacao: Por que a senhora bateu no meu filho? Ele foi mal-educado, e me chamou de gorda. E a senhora acha que vai emagrecer batendo nele?";
+    }
+
+    public String boaPiada3() {
+        return "Conversa de casados: Querido, o que você prefere? Uma mulher bonita ou uma mulher inteligente? Nem uma, nem outra. Você sabe que eu só gosto de você.";
+    }
+
+    public String boaPiada4() {
+        return "A mulher comenta com o marido: Querido, hoje o relógio caiu da parede da sala e por pouco não bateu na cabeça da mamãe... Maldito relógio. Sempre atrasado...";
+    }
+
+    public String boaPiada5() {
+        return "O condenado à morte esperava a hora da execução, quando chegou o padre: Meu filho, vim trazer a palavra de Deus para você. Perda de tempo, seu padre. Daqui a pouco vou falar com Ele, pessoalmente. Algum recado?";
+    }
+
+    public String ruimPiada1() {
+        return "O que tem quatro patas e um braco?\n" +
+                " Um pit-bull feliz. ??????";
+    }
+
+    public String ruimPiada2() {
+        return "Qual a punicao por bigamia?\n" +
+                " Duas sogras. ??";
+    }
+
+    public String ruimPiada3() {
+        return "O que um tijolo falou pro outro?\n" +
+                " Há um ciumento entre nos. ?";
+    }
+
+    public String ruimPiada4() {
+        return "Por que o Manoel ficou duas horas olhando fixamente pra lata de suco de laranja?\n" +
+                " Porque estava escrito “concentrado”. ??????";
+    }
+
+    public String ruimPiada5() {
+        return "Qual o tipo de alimento de que o político mais gosta?\n" +
+                " As massas. ????";
+    }
+
+}
